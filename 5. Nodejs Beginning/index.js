020771ca13228c1c60b871cf8e74e924eddd0e9b
as1001 <ashay165@gmail.com>
2023-01-11 01:23:52 +0530

    intro

--- 5. Nodejs Beginning/index.js
+++ 5. Nodejs Beginning/index.js
@@ -1,3 +1,5 @@
+
+
 console.log("Hello World!!");
 
 function add(a, b){
@@ -8,14 +10,16 @@ console.log("Addition is: ", add(2, 5));
 
 console.log(add);
 
-console.log(process.argv);
+console.log("process.argv:",process.argv);
 
 var args = process.argv.slice(2);
 
 console.log("Addition is: ", add(parseInt(args[0]), parseInt(args[1])));
 
 
-
+const operations = require('./operations')
+console.log("Addition from operation module: ", operations.add(4,6));
+console.log("Multiplication from operation module: ", operations.multiply(4,6));
 
 /*
 
